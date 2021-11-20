05bb9e0d29bae269cfa348eaf8321a347530cc10
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-11-20 05:29:14 +0000

    commit sync training start exp2 learning rate 0.0005

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 export GOOGLE_APPLICATION_CREDENTIALS="key.json"
 PROJECT_ID=$(gcloud config list project --format "value(core.project)")
 BUCKET_NAME=${PROJECT_ID}-aiplatform
-LOGDIR="assets/out/models"
+LOGDIR="assets/out/models/exp2"
 TIMESTEPS=1000000
 BATCH_SIZE=128
 MAX_EPISODE_SIZE=2500

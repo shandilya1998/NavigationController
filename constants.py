8edd8e9d8091f6c4faf448a8e01ab46caf67f4ad
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-12-14 22:10:42 +0530

    commit sync

--- constants.py
+++ constants.py
@@ -31,7 +31,7 @@ params = {
     'render_freq'                 : 20000,
     'save_freq'                   : 20000,
     'eval_freq'                   : 20000,
-    'buffer_size'                 : int(3.5e5),
+    'buffer_size'                 : int(4e5),
     'total_timesteps'             : int(1e6),
     'ds'                          : 0.01,
     'motor_cortex'                : [256, 128],

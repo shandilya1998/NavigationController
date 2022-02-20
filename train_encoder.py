e6d1289c8d8a75562daa10f71c4e5fa58e3b274d
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2022-02-20 14:25:19 +0530

    commit fix log directory

--- train_encoder.py
+++ train_encoder.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         ])
     )
 
-    logdir = '/content/drive/MyDrive/CNS/exp22/autoencoder/99d1ade' 
+    logdir = '/content/drive/MyDrive/CNS/exp22/autoencoder/36762d7' 
     #logdir = 'assets/out/models/autoencoder/'
 
     train_autoencoder(

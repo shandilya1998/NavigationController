980317e578e20533c71b5cfb697879ce3aa5e3f9
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2022-03-13 11:30:35 +0530

    commit exp22/TD3_31 setup

--- simulations/maze_env.py
+++ simulations/maze_env.py
@@ -824,7 +824,7 @@ class MazeEnv(gym.Env):
 
         # Computing the reward in "https://ieeexplore.ieee.org/document/8398461"
         goal = self._task.goals[self._task.goal_index].pos - self.wrapped_env.get_xy()
-        rho = (1 - np.linalg.norm(goal) / np.linalg.norm(self._task.goals[self._task.goal_index].pos)) * 0.1
+        rho = (1 - (np.linalg.norm(goal) / np.linalg.norm(self._task.goals[self._task.goal_index].pos))) * 0.1
         self.goals.pop(0)
         self.goals.append(goal)
         theta_t = self.check_angle(np.arctan2(goal[1], goal[0]) - self.get_ori())
@@ -837,8 +837,7 @@ class MazeEnv(gym.Env):
 
         # Task Reward Computation
         outer_reward = 0
-        if self.total_steps > params['stage1']:
-            outer_reward = self._task.reward(next_pos, bool(next_obs['inframe'][0])) + rho
+        outer_reward = self._task.reward(next_pos, bool(next_obs['inframe'][0])) + rho
         done = self._task.termination(self.wrapped_env.get_xy(),  bool(next_obs['inframe'][0]))
         info["position"] = self.wrapped_env.get_xy()
 

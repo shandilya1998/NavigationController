257bda7f1bcb0192c0a74be9b2b98802bfde1429
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2022-04-14 19:59:39 +0530

    commut sync

--- constants.py
+++ constants.py
@@ -62,7 +62,7 @@ params = {
     'gamma'                       : 0.98,
     'tau'                         : 0.002, 
     'n_updates'                   : 32,
-    'num_ctx'                     : 400,
+    'num_ctx'                     : 900,
     'actor_lr'                    : 1e-3,
     'critic_lr'                   : 1e-2,
     'weight_decay'                : 1e-2,

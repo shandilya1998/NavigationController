3242e43daecea56df9b032e20b054cee78ef1aba
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2023-04-10 08:47:51 +0530

    commit updation of cpu Dockerfile for installation of ROS and gstreamer, and for library version update

--- docker/cpu/build.sh
+++ docker/cpu/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+
 start=`date +%s`
-nohup docker build -t neurorobotics . > build.log &
+if [ $1 == "arm" ]
+then
+	nohup docker build -t neurorobotics arm/ > build.log &
+else
+	nohup docker build -t neurorobotics x86_64/ > build.log &
+fi
 end=`date +%s`
 runtime=$((end-start))
 echo "Build Runtime:" > build.log
-echo $(runtime) > build.log
+echo $runtime > build.log
 echo "Done. Thank you."
